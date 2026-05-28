bccc32c626eaa56b6876b1f96a74d8483e6e26d5
NehalMaheshwari <nehalm81005@gmail.com>
2026-05-29 00:00:22 +0530

    leetcode 181
    Added an optimized version of the query using INNER JOIN.

--- employees_more_than_manager.sql
+++ employees_more_than_manager.sql
@@ -1,6 +1,15 @@
-#LeetCode Problem #181 — Employees Earning More Than Their Managers
+#LeetCode #181 — Employees Earning More Than Their Managers
 SELECT e.name AS Employee
 FROM Employee e
 JOIN Employee m
 ON e.managerId = m.id
 WHERE e.salary > m.salary;
+
+#optimized query
+SELECT e.name AS Employee
+FROM Employee e
+INNER JOIN Employee m
+ON e.managerId = m.id
+WHERE e.salary > m.salary;
+
+#inner join
